678eb19bfa8b780d3d801d5175d5f716100d6532
Safayet <hossainsafayet187@gmail.com>
2024-05-19 22:36:43 +0600

    deploy

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['fakestoreapi.com'], // Add the hostname to the domains array
+    },
+  };
+  
+  module.exports = nextConfig;
+  
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+});
